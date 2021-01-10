c4de7064b8d98b7bde7d08d89fcc6eb03fae2853
egorka.kamenev17@gmail.com <egorka.kamenev17@gmail.com>
2021-01-11 00:11:55 +0700

    add save in json

---
+++ kursovaya.Core/CDataManager.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+
+namespace kursovaya.Core
+{
+    class CDataManager
+    {
+        static readonly string fileName = "../../../Data/Data.json";
+
+        public static ObservableCollection<Cmem> Memes { get; set; }
+
+        public class Data
+        {
+            public ObservableCollection<Cmem> Memes { get; set; }
+
+        }
+
+        /// <summary>
+        /// Функция загружает данные в память программы 
+        /// </summary>
+        public static void LoadData()
+        {
+            var data = Deserialize<Data>(fileName);
+            if (data != null)
+                Memes = data.Memes;
+            else
+                Memes = new ObservableCollection<Cmem>();
+        }
+        /// <summary>
+        /// Функция сохраняет данные в json файл
+        /// </summary>
+        public static void SaveData()
+        {
+            var data = new Data
+            {
+                Memes = CDataManager.Memes
+            };
+            Serialize(fileName, data);
+        }
+        /// <summary>
+        /// Функция которая читает файл  
+        /// </summary>
+        /// <typeparam name="T"> Тип данных поданых который нужно вернуть </typeparam>
+        /// <param name="filename">Название файла,который нужно прочитать</param>
+        /// <returns></returns>
+        private static T Deserialize<T>(string filename)
+        {
+            using var sr = new StreamReader(filename);
+            using var jsonReader = new JsonTextReader(sr);
+            var serializer = new JsonSerializer();
+            return serializer.Deserialize<T>(jsonReader);
+        }
+        /// <summary>
+        /// Сохранение файла
+        /// </summary>
+        /// <typeparam name="T">тип данных,которые нужно сохранить</typeparam>
+        /// <param name="fileName">Название файла,который нужно прочитать</param>
+        /// <param name="data">какие данные нужно сохранить</param>
+        private static void Serialize<T>(string fileName, T data)
+        {
+            using var sw = new StreamWriter(fileName);
+            using var jsonWriter = new JsonTextWriter(sw);
+            var serializer = new JsonSerializer();
+            serializer.Serialize(jsonWriter, data);
+
+        }
+    }
+}
--- kursovaya/Cmem.cs
+++ kursovaya/Cmem.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
@@ -13,31 +12,5 @@ namespace kursovaya
 {
     public class Cmem
     {
-        public string Name { get; set; }
-        public string Category { get; set; }
-        public string Source { get; set; }
-        public string HashTag { get; set; }
-
-        public static string filePath = "../../../../Memes/";
-
-        public Cmem(string name, string category, string source)
-        {
-            Name = name;
-            Category = category;
-            Source = source;
-        }
-
-        //функция скачивания мема из интернета по URL
-        public static bool DownloadMem(Uri url, string filename)
-        {
-            using WebClient client = new WebClient();
-            try
-            {
-                client.DownloadFile(url, filePath + '\\' + filename + ".png");
-                return true;
-            }
-            catch { return false; }
-        }
-
     }
 }
