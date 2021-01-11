77d212c297548014153b446b5f3aec916102bb0c
Сергей Фёдоров <rodjer99@bk.ru>
2021-01-11 19:02:09 +0700

    add parameters comand (павраметры комманд добавил)

---
+++ kursovaya.Core/ViewModels/Base/CRelayParametrizedCommands.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+{
+    /// <summary>
+    /// Базовая класс, который запускает работу
+    /// </summary>
+    public class CRelayParametrizedCommands : ICommand
+    {
+        /// <summary>
+        /// Запуск действий
+        /// </summary>
+        private readonly Action<object> mAction;
+
+        /// <summary>
+        /// Событие, которое срабатывает, когда <see cref="CanExecute(object)"/> значение изменилось
+        /// </summary>
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+
+        /// <summary>
+        /// стандартный конструктор
+        /// </summary>
+        public CRelayParametrizedCommands(Action<object> action)
+        {
+            mAction = action;
+        }
+
+        /// <summary>
+        /// Команда ретрансляции всегда может быть выполнена
+        /// </summary>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Выполняет действие команды
+        /// </summary>
+        public void Execute(object parameter)
+        {
+            mAction(parameter);
+        }
+    }
+}
---
+++ kursovaya.Core/ViewModels/CmainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Input;
+
+namespace kursovaya.Core.ViewModels
+{
+    public class CmainViewModel
+    {
+        public ObservableCollection<Cmem> Memes { get; set; }
+
+        public string MemeName { get; set; }
+        public string MemeCategory { get; set; }
+        public string MemeLocation { get; set; }
+
+        public string MemeHashTag { get; set; } = "#";
+        public Uri MemeUrl { get; set; }
+        public string SearchString { get; set; }
+
+        public ICommand DeleteMemeCommand { get; set; }
+        public ICommand AddNewMemeCommand { get; set; }
+        public ICommand SearchCommand { get; set; }
+
+        public CmainViewModel()
+        {
+
+        }
+
+            
+        
+    }
+}
